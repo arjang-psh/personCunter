aa47c5ff2bdc101c875b878309c124fe14c4297b
Arjang Pezeshki <arjangpezeshki@Arjangs-MacBook-Pro.local>
2022-07-23 05:08:20 -0700

    helper wrapper is added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
-
+import Wrapper from '../Helpers/Wrapper';
 import Button from '../UI/Button';
 import ErrorModal from './ErrorModal';
 
@@ -46,7 +46,7 @@ const AddUser = props => {
     }
 
     return (
-        <>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -57,7 +57,7 @@ const AddUser = props => {
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
-        </>
+        </Wrapper>
     )
 }
 
